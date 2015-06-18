e0a049fe8d8fd1dcaf4825559e6b169c2b91f6d9
piggy_xrh <piggy_xrh@163.com>
2015-06-17 23:40:08 -0700

    add @crttime to record the time to create thread

--- src/tpool.c
+++ src/tpool.c
@@ -2288,9 +2288,9 @@ tpool_get_restto(struct tpool_t *pool, struct tpool_thread_t *self) {
 		
 		/* Adjust the rest time according to the time to create the latest thread  */
 		assert(self->last_to >= 0);
-		if (nt >= pool->crttime) 
+		if (nt <= pool->crttime) 
 			pool->crttime = nt;
-		t_recomment = 5000 - 1000 * (nt - pool->crttime);
+		t_recomment = 4600 - 1000 * (nt - pool->crttime);
 
 		while (self->last_to < t_recomment)
 			self->last_to += t_recomment / 4;
